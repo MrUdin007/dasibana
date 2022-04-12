4a2f8950d3cbd092b9a08a6ccf0332d6403a9a86
MrUdin007 <cd C:\Program Files\Git\bin>
2022-04-12 23:46:45 +0700

    add new product page

---
+++ resources/views/layouts/be/app.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!-- CSRF Token -->
+        <title>Dasibana - Admin</title>
+
+        <!-- ****** faviconit.com favicons ****** -->
+        <link rel="apple-touch-icon" sizes="57x57" href="{{asset('favicon/apple-icon-57x57.png')}}">
+        <link rel="apple-touch-icon" sizes="60x60" href="{{asset('favicon/apple-icon-60x60.png')}}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{asset('favicon/apple-icon-72x72.png')}}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{asset('favicon/apple-icon-76x76.png')}}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{asset('favicon/apple-icon-114x114.png')}}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{asset('favicon/apple-icon-120x120.png')}}">
+        <link rel="apple-touch-icon" sizes="144x144" href="{{asset('favicon/apple-icon-144x144.png')}}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{asset('favicon/apple-icon-152x152.png')}}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-icon-180x180.png')}}">
+        <link rel="icon" type="image/png" sizes="192x192"  href="{{asset('favicon/android-icon-192x192.png')}}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon/favicon-32x32.png')}}">
+        <link rel="icon" type="image/png" sizes="96x96" href="{{asset('favicon/favicon-96x96.png')}}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
+        <link rel="manifest" href="{{asset('favicon/manifest.json')}}">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="{{asset('favicon/ms-icon-144x144.png')}}">
+        <meta name="theme-color" content="#ffffff">
+        <!-- ****** faviconit.com favicons ****** -->
+
+        <link href="{{ asset('dist/be/css/main.css') }}" rel="stylesheet">
+        <link href="{{ asset('dist/be/vendor/css/style.css') }}" rel="stylesheet">
+        <link href="{{ asset('dist/be/vendor/datatables/rowReorder.dataTables.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('dist/vendors/css/bootstrap.css') }}" rel="stylesheet">
+        <link href="{{ asset('dist/be/vendor/css/sweetalert.css') }}" rel="stylesheet">
+        <link href="{{ asset('dist/vendors/select2/dist/css/select2.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('dist/be/vendor/datatables/jquery.dataTables.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('dist/fe/css/content/floating_input.css') }}" rel="stylesheet">
+        @stack('css')
+    </head>
+    <body class="app">
+        <div id='loader'>
+            <div class="spinner"></div>
+        </div>
+
+        <!-- @App Content -->
+        <!-- =================================================== -->
+        <div>
+            <!-- Start: Sidebar -->
+            <!-- ============================================================== -->
+            @include('layouts.be.sidebar')
+            <!-- ============================================================== -->
+            <!-- End:/ Sidebar -->
+
+            <!-- #Main ============================ -->
+            <div class="page-container">
+                <!-- Start: Header -->
+                <!-- ============================================================== -->
+                @include('layouts.be.header')
+                <!-- ============================================================== -->
+                <!-- End:/ Header -->
+
+                <!-- ### $App Screen Content ### -->
+                <main class='main-content bgc-grey-100'>
+                    @yield('content')
+                </main>
+
+                <!-- Start: Footer -->
+                <!-- ============================================================== -->
+                @include('layouts.be.footer')
+                <!-- ============================================================== -->
+                <!-- End:/ Footer -->
+            </div>
+        </div>
+
+        <!-- ============================================================== -->
+        <!-- All Jquery -->
+        <!-- ============================================================== -->
+        <!-- <script type="text/javascript" src="{{ asset('dist/be/js/app.js') }}"></script> -->
+        <script type="text/javascript" src="{{ asset('dist/be/vendor/jquery/dist/jquery.min.js') }}"></script>
+        <!-- <script type="text/javascript" src="{{ asset('dist/be/vendor/js/moment.min.js') }}"></script> -->
+        <script type="text/javascript" src="{{ asset('dist/vendors/js/bootstrap.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('dist/vendors/js/hide_input.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('dist/be/vendor/js/vendor.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('dist/be/vendor/js/bundle.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('dist/be/vendor/js/sweetalert.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('dist/be/vendor/datatables/dataTables.rowReorder.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('dist/be/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('dist/vendors/select2/dist/js/select2.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('dist/be/vendor/ckeditor4/ckeditor.js') }}"></script>
+        <script type="text/javascript">
+            window.addEventListener('load', function load() {
+                const loader = document.getElementById('loader');
+                setTimeout(function() {
+                    loader.classList.add('fadeOut');
+                }, 300);
+            });
+            $(document).ready(function () {
+                $('#dropdownVeganesia').click(function(){
+                    $('#menudropdownheader').toggle();
+                });
+
+                /*start: Select2 */
+                $(".select2").select2({
+                    placeholder: function(){
+                        $(this).data('placeholder');
+                    }
+                });
+                /*End:/ Select2 */
+
+                $("#tesmodal").click(function(){
+                    $("#exampleModal").modal('show');
+                });
+
+                /*start: CkEditor Example */
+                CKEDITOR.replace('tesCkeditor', {
+                    filebrowserUploadUrl: '/uploader/upload.php?type=Files'
+                });
+                /*end:/ CkEditor Example */
+
+                $('.logout-veg').click(function () {
+                    $('#formLogout').submit();
+                });
+
+            });
+
+        </script>
+        @stack('scripts')
+    </body>
+</html>
---
+++ resources/views/layouts/be/sidebar.blade.php
@@ -0,0 +1,77 @@
+<div class="sidebar">
+    <div class="sidebar-inner">
+        <!-- ### $Sidebar Header ### -->
+        <div class="sidebar-logo">
+            <div class="peers ai-c fxw-nw">
+                <div class="peer peer-greed">
+                    <a class="sidebar-link td-n" href="">
+                        <div class="peers ai-c fxw-nw">
+                            <div class="peer">
+                                <div class="logo">
+                                    <img class="logo-vgn" src="" alt="">
+                                </div>
+                            </div>
+                            <div class="peer peer-greed">
+                                <h5 class="lh-1 mB-0 logo-text logo-text-vegan f-pattaya_reg">Veganesia</h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="peer">
+                    <div class="mobile-toggle sidebar-toggle">
+                        <a href="" class="td-n">
+                            <i class="ti-arrow-circle-left"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ### $Sidebar Menu ### -->
+        <ul class="sidebar-menu scrollable pos-r">
+            <li class="nav-item">
+                <a class="sidebar-link" href="">
+                    <span class="icon-holder">
+                        <i class="c-blue-500 ti-home"></i>
+                    </span>
+                    <span class="title">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="sidebar-link" href="">
+                    <span class="icon-holder">
+                        <i class="c-blue-500 ti-user"></i>
+                    </span>
+                    <span class="title">Edit Profile</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder">
+                        <i class="c-teal-500 ti-bell"></i>
+                    </span>
+                    <span class="title">Notifications</span>
+                    <span class="arrow">
+                        <i class="ti-angle-right"></i>
+                    </span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li class="nav-item dropdown">
+                        <a href="" class="">
+                            <span>New Orders</span>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="">
+                            <span>New Payment Proof</span>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="" class="">
+                            <span>Stock Habis</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
