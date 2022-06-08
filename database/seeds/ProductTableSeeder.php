8af4e0d0e03ff54114ec712211c410c6ecaedfa2
MrUdin007 <cd C:\Program Files\Git\bin>
2022-06-08 13:37:05 +0700

    add product images

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -16,70 +16,70 @@ class ProductTableSeeder extends Seeder
                 'name'          =>  'product1',
                 'category_id'   =>  '1',
                 'status'        =>  true,
-                'foto'          =>  '',
+                'foto'          =>  'images/product1.jpg',
                 'slug'          =>  'admin1'
             ],
             [
                 'name'          =>  'product2',
                 'category_id'   =>  '2',
                 'status'        =>  true,
-                'foto'          =>  '',
+                'foto'          =>  'images/product2.jpg',
                 'slug'          =>  'admin2'
             ],
             [
                 'name'          =>  'product3',
                 'category_id'   =>  '3',
                 'status'        =>  true,
-                'foto'          =>  '',
+                'foto'          =>  'images/product3.jpg',
                 'slug'          =>  'admin3'
             ],
             [
                 'name'          =>  'product4',
                 'category_id'   =>  '1',
                 'status'        =>  true,
-                'foto'          =>  '',
+                'foto'          =>  'images/product4.jpg',
                 'slug'          =>  'admin4'
             ],
             [
                 'name'          =>  'product5',
                 'category_id'   =>  '2',
                 'status'        =>  true,
-                'foto'          =>  '',
+                'foto'          =>  'images/product5.jpg',
                 'slug'          =>  'admin5'
             ],
             [
                 'name'          =>  'product6',
                 'category_id'   =>  '3',
                 'status'        =>  false,
-                'foto'          =>  '',
+                'foto'          =>  'images/product6.jpg',
                 'slug'          =>  'admin6'
             ],
             [
                 'name'          =>  'product7',
                 'category_id'   =>  '1',
                 'status'        =>  false,
-                'foto'          =>  '',
+                'foto'          =>  'images/product7.jpg',
                 'slug'          =>  'admin7'
             ],
             [
                 'name'          =>  'product8',
                 'category_id'   =>  '2',
                 'status'        =>  false,
-                'foto'          =>  '',
+                'foto'          =>  'images/product8.jpg',
                 'slug'          =>  'admin8'
             ],
             [
                 'name'          =>  'product9',
                 'category_id'   =>  '3',
                 'status'        =>  false,
-                'foto'          =>  '',
+                'foto'          =>  'images/product9.jpg',
                 'slug'          =>  'admin9'
             ],
             [
                 'name'          =>  'product10',
                 'category_id'   =>  '1',
                 'status'        =>  false,
-                'foto'          =>  '',
+                'foto'          =>  'images/product10.jpg',
                 'slug'          =>  'admin10'
             ]
         ]);
