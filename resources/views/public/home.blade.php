2c67e80a650fda4160bfd4d4a32d7ed1b391fa8a
risnasainudin <rsainudin@dgitsystems.com>
2022-06-19 15:32:45 +0800

    add hover check cart

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -25,34 +25,114 @@
                 <h1 class="main-title">produk unggulan</h1>
                 <div class="gr_display gr-prodc">
                     <div class="produk-item">
-                        <div class="pic-produk" style="background-image: url('{{asset('images/product1.jpg')}}')"></div>
+                        <div class="pic-produk" style="background-image: url('{{asset('images/product1.jpg')}}')">
+                            <div class="bottom-hidden-btn">
+                                <div class="gr_display gr-btn-prdc">
+                                    <a class="pc-btn-prdct --shopee" target="_blank" href="https://shopee.co.id/Dasii-Kupu-kupu-Hitam-Polos-Mengkilap-TOP-QUALITY-i.35300544.592150122?sp_atk=301a5d17-6012-4ad1-af69-f937d8c0244a&xptdk=301a5d17-6012-4ad1-af69-f937d8c0244a" style="background-image: url('{{asset('images/icons/shopee.png')}}')"></a>
+                                    <a class="pc-btn-prdct --tokped" target="_blank" href="https://www.tokopedia.com/konveksibana/dasi-panjang-import-lurik" style="background-image: url('{{asset('images/icons/tokopedia.png')}}')"></a>
+                                </div>
+                                <div class="overlay-btns-prd"></div>
+                            </div>
+                        </div>
                     </div>
                     <div class="produk-item">
-                        <div class="pic-produk" style="background-image: url('{{asset('images/product5.jpg')}}')"></div>
+                        <div class="pic-produk" style="background-image: url('{{asset('images/product5.jpg')}}')">
+                            <div class="bottom-hidden-btn">
+                                <div class="gr_display gr-btn-prdc">
+                                    <a class="pc-btn-prdct --shopee" target="_blank" href="https://shopee.co.id/Dasii-Kupu-kupu-Hitam-Polos-Mengkilap-TOP-QUALITY-i.35300544.592150122?sp_atk=301a5d17-6012-4ad1-af69-f937d8c0244a&xptdk=301a5d17-6012-4ad1-af69-f937d8c0244a" style="background-image: url('{{asset('images/icons/shopee.png')}}')"></a>
+                                    <a class="pc-btn-prdct --tokped" target="_blank" href="https://www.tokopedia.com/konveksibana/dasi-panjang-import-lurik" style="background-image: url('{{asset('images/icons/tokopedia.png')}}')"></a>
+                                </div>
+                                <div class="overlay-btns-prd"></div>
+                            </div>
+                        </div>
                     </div>
                     <div class="produk-item">
-                        <div class="pic-produk" style="background-image: url('{{asset('images/product8.jpg')}}')"></div>
+                        <div class="pic-produk" style="background-image: url('{{asset('images/product8.jpg')}}')">
+                            <div class="bottom-hidden-btn">
+                                <div class="gr_display gr-btn-prdc">
+                                    <a class="pc-btn-prdct --shopee" target="_blank" href="https://shopee.co.id/Dasii-Kupu-kupu-Hitam-Polos-Mengkilap-TOP-QUALITY-i.35300544.592150122?sp_atk=301a5d17-6012-4ad1-af69-f937d8c0244a&xptdk=301a5d17-6012-4ad1-af69-f937d8c0244a" style="background-image: url('{{asset('images/icons/shopee.png')}}')"></a>
+                                    <a class="pc-btn-prdct --tokped" target="_blank" href="https://www.tokopedia.com/konveksibana/dasi-panjang-import-lurik" style="background-image: url('{{asset('images/icons/tokopedia.png')}}')"></a>
+                                </div>
+                                <div class="overlay-btns-prd"></div>
+                            </div>
+                        </div>
                     </div>
                     <div class="produk-item">
-                        <div class="pic-produk" style="background-image: url('{{asset('images/product1.jpg')}}')"></div>
+                        <div class="pic-produk" style="background-image: url('{{asset('images/product1.jpg')}}')">
+                            <div class="bottom-hidden-btn">
+                                <div class="gr_display gr-btn-prdc">
+                                    <a class="pc-btn-prdct --shopee" target="_blank" href="https://shopee.co.id/Dasii-Kupu-kupu-Hitam-Polos-Mengkilap-TOP-QUALITY-i.35300544.592150122?sp_atk=301a5d17-6012-4ad1-af69-f937d8c0244a&xptdk=301a5d17-6012-4ad1-af69-f937d8c0244a" style="background-image: url('{{asset('images/icons/shopee.png')}}')"></a>
+                                    <a class="pc-btn-prdct --tokped" target="_blank" href="https://www.tokopedia.com/konveksibana/dasi-panjang-import-lurik" style="background-image: url('{{asset('images/icons/tokopedia.png')}}')"></a>
+                                </div>
+                                <div class="overlay-btns-prd"></div>
+                            </div>
+                        </div>
                     </div>
                     <div class="produk-item">
-                        <div class="pic-produk" style="background-image: url('{{asset('images/product1.jpg')}}')"></div>
+                        <div class="pic-produk" style="background-image: url('{{asset('images/product1.jpg')}}')">
+                            <div class="bottom-hidden-btn">
+                                <div class="gr_display gr-btn-prdc">
+                                    <a class="pc-btn-prdct --shopee" target="_blank" href="https://shopee.co.id/Dasii-Kupu-kupu-Hitam-Polos-Mengkilap-TOP-QUALITY-i.35300544.592150122?sp_atk=301a5d17-6012-4ad1-af69-f937d8c0244a&xptdk=301a5d17-6012-4ad1-af69-f937d8c0244a" style="background-image: url('{{asset('images/icons/shopee.png')}}')"></a>
+                                    <a class="pc-btn-prdct --tokped" target="_blank" href="https://www.tokopedia.com/konveksibana/dasi-panjang-import-lurik" style="background-image: url('{{asset('images/icons/tokopedia.png')}}')"></a>
+                                </div>
+                                <div class="overlay-btns-prd"></div>
+                            </div>
+                        </div>
                     </div>
                     <div class="produk-item">
-                        <div class="pic-produk" style="background-image: url('{{asset('images/product5.jpg')}}')"></div>
+                        <div class="pic-produk" style="background-image: url('{{asset('images/product5.jpg')}}')">
+                            <div class="bottom-hidden-btn">
+                                <div class="gr_display gr-btn-prdc">
+                                    <a class="pc-btn-prdct --shopee" target="_blank" href="https://shopee.co.id/Dasii-Kupu-kupu-Hitam-Polos-Mengkilap-TOP-QUALITY-i.35300544.592150122?sp_atk=301a5d17-6012-4ad1-af69-f937d8c0244a&xptdk=301a5d17-6012-4ad1-af69-f937d8c0244a" style="background-image: url('{{asset('images/icons/shopee.png')}}')"></a>
+                                    <a class="pc-btn-prdct --tokped" target="_blank" href="https://www.tokopedia.com/konveksibana/dasi-panjang-import-lurik" style="background-image: url('{{asset('images/icons/tokopedia.png')}}')"></a>
+                                </div>
+                                <div class="overlay-btns-prd"></div>
+                            </div>
+                        </div>
                     </div>
                     <div class="produk-item">
-                        <div class="pic-produk" style="background-image: url('{{asset('images/product8.jpg')}}')"></div>
+                        <div class="pic-produk" style="background-image: url('{{asset('images/product8.jpg')}}')">
+                            <div class="bottom-hidden-btn">
+                                <div class="gr_display gr-btn-prdc">
+                                    <a class="pc-btn-prdct --shopee" target="_blank" href="https://shopee.co.id/Dasii-Kupu-kupu-Hitam-Polos-Mengkilap-TOP-QUALITY-i.35300544.592150122?sp_atk=301a5d17-6012-4ad1-af69-f937d8c0244a&xptdk=301a5d17-6012-4ad1-af69-f937d8c0244a" style="background-image: url('{{asset('images/icons/shopee.png')}}')"></a>
+                                    <a class="pc-btn-prdct --tokped" target="_blank" href="https://www.tokopedia.com/konveksibana/dasi-panjang-import-lurik" style="background-image: url('{{asset('images/icons/tokopedia.png')}}')"></a>
+                                </div>
+                                <div class="overlay-btns-prd"></div>
+                            </div>
+                        </div>
                     </div>
                     <div class="produk-item">
-                        <div class="pic-produk" style="background-image: url('{{asset('images/product1.jpg')}}')"></div>
+                        <div class="pic-produk" style="background-image: url('{{asset('images/product1.jpg')}}')">
+                            <div class="bottom-hidden-btn">
+                                <div class="gr_display gr-btn-prdc">
+                                    <a class="pc-btn-prdct --shopee" target="_blank" href="https://shopee.co.id/Dasii-Kupu-kupu-Hitam-Polos-Mengkilap-TOP-QUALITY-i.35300544.592150122?sp_atk=301a5d17-6012-4ad1-af69-f937d8c0244a&xptdk=301a5d17-6012-4ad1-af69-f937d8c0244a" style="background-image: url('{{asset('images/icons/shopee.png')}}')"></a>
+                                    <a class="pc-btn-prdct --tokped" target="_blank" href="https://www.tokopedia.com/konveksibana/dasi-panjang-import-lurik" style="background-image: url('{{asset('images/icons/tokopedia.png')}}')"></a>
+                                </div>
+                                <div class="overlay-btns-prd"></div>
+                            </div>
+                        </div>
                     </div>
                     <div class="produk-item">
-                        <div class="pic-produk" style="background-image: url('{{asset('images/product8.jpg')}}')"></div>
+                        <div class="pic-produk" style="background-image: url('{{asset('images/product8.jpg')}}')">
+                            <div class="bottom-hidden-btn">
+                                <div class="gr_display gr-btn-prdc">
+                                    <a class="pc-btn-prdct --shopee" target="_blank" href="https://shopee.co.id/Dasii-Kupu-kupu-Hitam-Polos-Mengkilap-TOP-QUALITY-i.35300544.592150122?sp_atk=301a5d17-6012-4ad1-af69-f937d8c0244a&xptdk=301a5d17-6012-4ad1-af69-f937d8c0244a" style="background-image: url('{{asset('images/icons/shopee.png')}}')"></a>
+                                    <a class="pc-btn-prdct --tokped" target="_blank" href="https://www.tokopedia.com/konveksibana/dasi-panjang-import-lurik" style="background-image: url('{{asset('images/icons/tokopedia.png')}}')"></a>
+                                </div>
+                                <div class="overlay-btns-prd"></div>
+                            </div>
+                        </div>
                     </div>
                     <div class="produk-item">
-                        <div class="pic-produk" style="background-image: url('{{asset('images/product1.jpg')}}')"></div>
+                        <div class="pic-produk" style="background-image: url('{{asset('images/product1.jpg')}}')">
+                            <div class="bottom-hidden-btn">
+                                <div class="gr_display gr-btn-prdc">
+                                    <a class="pc-btn-prdct --shopee" target="_blank" href="https://shopee.co.id/Dasii-Kupu-kupu-Hitam-Polos-Mengkilap-TOP-QUALITY-i.35300544.592150122?sp_atk=301a5d17-6012-4ad1-af69-f937d8c0244a&xptdk=301a5d17-6012-4ad1-af69-f937d8c0244a" style="background-image: url('{{asset('images/icons/shopee.png')}}')"></a>
+                                    <a class="pc-btn-prdct --tokped" target="_blank" href="https://www.tokopedia.com/konveksibana/dasi-panjang-import-lurik" style="background-image: url('{{asset('images/icons/tokopedia.png')}}')"></a>
+                                </div>
+                                <div class="overlay-btns-prd"></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
