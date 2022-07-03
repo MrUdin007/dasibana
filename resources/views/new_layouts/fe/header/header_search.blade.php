987a214078040fb64a2c5527a16ed014f5984869
risnasainudin <rsainudin@dgitsystems.com>
2022-07-03 21:45:11 +0800

    delete unused blade

--- resources/views/new_layouts/fe/header/header_search.blade.php
+++
@@ -1,9 +0,0 @@
-@foreach($datas as $data)
-<a class="item-search-list" href="#">
-    <div class="grid-global-search --gr-prdct">
-        <div>
-            <h6>suggest search</h6>
-        </div>
-    </div>
-</a>
-@endforeach
